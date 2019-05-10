cd4719640c7b1ce4628c27a96bb2d2a581a34b88
joethorley <joe@poissonconsulting.ca>
2019-05-10 14:10:39 -0700

    test reset

--- tests/testthat/test-timer.R
+++ tests/testthat/test-timer.R
@@ -36,3 +36,31 @@ test_that("tmr_is_running",{
                "x must not be a missing value")
 })
 
+test_that("tmr_is_running",{
+  expect_false(tmr_is_running(tmr_timer()))
+  expect_true(tmr_is_running(tmr_timer(start = TRUE)))
+  expect_false(tmr_is_running(hms::as_hms(1)))
+  
+  expect_error(tmr_is_running(1), "x must be class hms")
+  expect_error(tmr_is_running(hms::as_hms(c(1, 2))),
+               "x must be a scalar")
+  expect_error(tmr_is_running(hms::as_hms(NA)),
+               "x must not be a missing value")
+})
+
+test_that("tmr_reset",{
+  
+  expect_identical(tmr_reset(tmr_timer(1)), tmr_timer())
+  expect_identical(tmr_reset(tmr_timer(1)), tmr_timer())
+
+  expect_true(tmr_is_running(tmr_timer(start = TRUE)))
+  expect_false(tmr_is_running(hms::as_hms(1)))
+  
+  expect_error(tmr_is_running(1), "x must be class hms")
+  expect_error(tmr_is_running(hms::as_hms(c(1, 2))),
+               "x must be a scalar")
+  expect_error(tmr_is_running(hms::as_hms(NA)),
+               "x must not be a missing value")
+})
+
+
