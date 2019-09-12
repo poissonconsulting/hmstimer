3f82899546c805f2ed90cbbf802eeb2444136dd9
joethorley <joe@poissonconsulting.ca>
2019-09-12 08:23:30 -0700

    length != 1L

--- R/check.R
+++ R/check.R
@@ -2,7 +2,7 @@ check_seconds <- function(seconds) {
   if(is.numeric(seconds) && length(seconds) == 1L && !is.na(seconds))
     return(invisible())
   if (!is.numeric(seconds)) err("`seconds` must be class numeric.")
-  if (!identical(length(seconds), 1L)) err("`seconds` must be a scalar.")
+  if (length(seconds) != 1L) err("`seconds` must be a scalar.")
   err("`seconds` must not be a missing value.")
 }
 
@@ -10,7 +10,7 @@ check_start <- function(start) {
   if(is.logical(start) && length(start) == 1L && !is.na(start))
     return(invisible())
   if (!is.logical(start)) err("`start` must be class logical.")
-  if (!identical(length(start), 1L)) err("`start` must be a scalar.")
+  if (length(start) != 1L) err("`start` must be a scalar.")
   err("`start` must not be a missing value.")
 }
 
@@ -19,6 +19,6 @@ check_x <- function(x) {
     return(invisible())
   
   if (!is.hms(x)) err("`x` must be class hms.")
-  if (!identical(length(x), 1L)) err("`x` must be a scalar.")
+  if (length(x) == 1L) err("`x` must be a scalar.")
   err("`x` must not be a missing value.")
 }
