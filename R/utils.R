47bf276aeeceb44e48c615b5483a3c41a12c1a69
joethorley <joe@poissonconsulting.ca>
2019-09-12 08:23:19 -0700

    not set domain mgs

--- R/utils.R
+++ R/utils.R
@@ -1,5 +1,5 @@
 is.hms <- function(x) inherits(x, "hms")
 
-err <- function(...) stop(..., call. = FALSE, domain = NA)
+err <- function(...) stop(..., call. = FALSE)
 
-wrn <- function(...) warning(..., call. = FALSE, domain = NA)
+wrn <- function(...) warning(..., call. = FALSE)
