ef9894cbf4433199cfe6ed0f42d159fc6f632221
Joe Thorley <joe@poissonconsulting.ca>
2021-09-20 08:18:34 -0700

    - Removed deprecated `start = FALSE` argument from `tmr_reset()`.

--- R/reset.R
+++ R/reset.R
@@ -10,12 +10,7 @@
 #' tmr <- tmr_timer(10)
 #' print(tmr)
 #' tmr_reset(tmr)
-tmr_reset <- function(x, seconds = 0, start = FALSE) {
-  if(!missing(start)) {
-    lifecycle::deprecate_stop(
-      "0.1.0", "tmr_reset(start = )", 
-      details = "Please stop or start the hms_timer before resetting.")
-  }
+tmr_reset <- function(x, seconds = 0) {
   start <- tmr_is_started(x)
   tmr_timer(seconds = seconds, start = start)
 }
--- tests/testthat/test-reset.R
+++ tests/testthat/test-reset.R
@@ -3,5 +3,4 @@ test_that("tmr_reset", {
   expect_true(tmr_is_stopped(tmr_reset(tmr_timer())))
   expect_true(tmr_is_started(tmr_reset(tmr_timer(start = TRUE))))
   expect_identical(tmr_reset(tmr_timer(1), 2), tmr_timer(2))
-  lifecycle::expect_defunct(tmr_reset(tmr_timer(1), start = FALSE))
 })
