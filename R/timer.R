1473ff7c9f460c3860405e74a9e78e75059f84fc
Joe Thorley <joe@poissonconsulting.ca>
2022-09-14 16:53:41 -0700

    adding internal tmr()

--- R/timer.R
+++ R/timer.R
@@ -16,16 +16,6 @@ tmr_timer <- function(seconds = 0, start = FALSE, title = TRUE) {
   chk_seconds(seconds)
   chk_start(start)
   chk_flag_or_string(title)
-
-  seconds <- as.double(seconds)
-
-  x <- as_hms(seconds)
-  if(isTRUE(title)) {
-    title <- force(gen_title())
-  }
-  if(!isFALSE(title)) {
-    x <- set_title(x, title)
-  }
-  if (start) x <- tmr_start(x)
-  x
+  
+  tmr(seconds, start, title)
 }
--- R/utils.R
+++ R/utils.R
@@ -4,8 +4,7 @@ err <- function(...) stop(..., call. = FALSE)
 
 wrn <- function(...) warning(..., call. = FALSE)
 
-gen_title <- function() {
-  caller <- sys.call(-2)
+gen_title <- function(caller = sys.call(-1)) {
   srcref <- utils::getSrcref(caller)
   file <- utils::getSrcFilename(caller)
   paste0(file, ":", srcref[[1]])
@@ -13,5 +12,19 @@ gen_title <- function() {
 
 set_title <- function(x, title) {
   attr(x, "title") <- title
+  x
+}
+
+tmr <- function(seconds, start, title, caller = sys.call(-1)) {
+  seconds <- as.double(seconds)
+
+  x <- as_hms(seconds)
+  if(isTRUE(title)) {
+    title <- force(gen_title(caller = caller))
+  }
+  if(!isFALSE(title)) {
+    x <- set_title(x, title)
+  }
+  if (start) x <- tmr_start(x)
   x
 }
