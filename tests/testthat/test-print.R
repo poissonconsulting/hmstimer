e79c703e3642ab643f4467baf972e9041ae8b58f
Joe Thorley <joe@poissonconsulting.ca>
2022-09-19 10:21:42 -0700

    test print

--- tests/testthat/test-print.R
+++ tests/testthat/test-print.R
@@ -1,12 +1,8 @@
 test_that("test tmr_print output", {
   tmr <- tmr_start(tmr_timer())
-  sys_time <- hms::as_hms(strftime(Sys.time(), format = "%H:%M:%S", tz = "UTC"))
-  start <- tmr_ceiling(hms::as_hms(sys_time - tmr_ceiling(tmr_elapsed(tmr))))
-  time_passed <- tmr_ceiling(tmr_elapsed(tmr))
-  output <- capture_output(tmr_print(tmr))
-  expect_match(output, paste("", start, " (+", time_passed, " => ", sys_time, ")", sep = ""), fixed = TRUE)
-  expect_type(output, "character")
-  })
+  Sys.sleep(0.1)
+  expect_match(capture_output(tmr_print(tmr)), "^\\[1\\] \"(\\d{2}:){2}\\d{2} \\(\\+(0{2}:){2}\\d{2} => (\\d{2}:){2}\\d{2}\\)\"$")
+})
 
 test_that("test class", {
   tmr <- tmr_start(tmr_timer())
