09de67296b9093275f8acd345f74b0df79ed20fb
Joe Thorley <joe@poissonconsulting.ca>
2024-08-08 08:35:00 -0700

    update params files

--- R/params.R
+++ R/params.R
@@ -1,12 +1,17 @@
-#' Default Parameter Descriptions
+#' Parameter Descriptions
 #'
 #' Default parameter descriptions which may be overridden in individual
 #' functions.
 #'
-#' @inheritParams rlang::args_dots_empty
-#' @param .local_envir The environment to use for scoping.
-#' @param code Code to time.
-#' @param digits A whole number of the number of decimal places.
+#' A flag is a non-missing logical scalar.
+#'
+#' A string is a non-missing character scalar.
+#' 
+#' A count is a non-missing non-negative integer scalar or double
+#' scalar with no fractional part.
+#
+#' @param code A line or block of R code.
+#' @param digits A count of the number of decimal places.
 #' @param print_title A flag specifying whether to print the title.
 #' @param seconds A non-negative numeric scalar of the initial number of seconds.
 #' @param srcref A flag specifying whether to print the source reference.
@@ -14,6 +19,9 @@
 #' @param title A string of the title.
 #' @param value A string of the title.
 #' @param x A [hms_timer()].
+#' @param .local_envir The environment to use for scoping.
+#' @inheritParams rlang::args_dots_empty
 #' @keywords internal
+#' @aliases parameters arguments args
 #' @usage NULL
 params <- function(...) NULL
